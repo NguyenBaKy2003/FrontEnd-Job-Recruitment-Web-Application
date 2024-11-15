3f5d460864585bc05add070e68499b44cb32e203
Nguyen Ba Ky <nguyenbaky23@gmail.com>
2024-11-15 11:36:46 +0700

    3

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   // const [count, setCount] = useState(0);
 
   return (
-    <div className="flex items-center justify-center h-screen bg-blue-500">
+    <div className="flex w-screen items-center justify-center h-screen bg-blue-500">
       <h1 className="text-4xl font-bold text-white">Hello, Tailwind CSS!</h1>
     </div>
   );
