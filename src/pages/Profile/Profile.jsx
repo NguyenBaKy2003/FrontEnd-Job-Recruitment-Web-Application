27dd852f9f44aaccfedd2c1fb86769f9c628e4f4
Nguyen Ba Ky <nguyenbaky23@gmail.com>
2024-12-15 00:28:28 +0700

    update

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -8,7 +8,8 @@ const RecruiterProfile = () => {
     website: "",
     github: "",
     facebook: "",
-    fullName: "",
+    firstName: "",
+    lastName: "",
     position: "",
     email: "",
     phone: "",
@@ -38,10 +39,19 @@ const RecruiterProfile = () => {
   // Handle input change to update the profile in state
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setProfile((prevProfile) => ({
-      ...prevProfile,
-      [name]: value,
-    }));
+    setProfile((prevProfile) => {
+      const updatedProfile = {
+        ...prevProfile,
+        [name]: value,
+      };
+
+      // Update fullName dynamically based on lastName + fullName
+      if (name === "lastName" || name === "fullName") {
+        updatedProfile.fullName = `${updatedProfile.lastName} ${value}`;
+      }
+
+      return updatedProfile;
+    });
   };
 
   return (
@@ -57,12 +67,15 @@ const RecruiterProfile = () => {
         <div className="bg-white p-6 rounded-lg shadow-lg">
           <div className="flex flex-col items-center">
             <img
-              src={profile.avatar || "https://via.placeholder.com/150"}
+              src={
+                profile.avatar ||
+                "https://scontent.fvii2-4.fna.fbcdn.net/v/t39.30808-6/344542573_1016776169700186_59734981782926054_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeHE3pzgZ09WIumVk2SaUlzW8AfSrMmpN6nwB9Ksyak3qRO3GRAQCJN5k0u9DAbLgHWd1mkLGJGTGZQ8xITqFMmC&_nc_ohc=glxuONPvYGoQ7kNvgEg-RXV&_nc_zt=23&_nc_ht=scontent.fvii2-4.fna&_nc_gid=AZDMc-cGe0uS9ooeE2tioMl&oh=00_AYDf-hi1OJg2v9JR8ZFocggASMj5dtc8Qzejloq9jHp0WA&oe=67638EC8"
+              }
               alt="Avatar"
               className="w-24 h-24 rounded-full mb-4"
             />
             <h2 className="text-xl font-semibold">
-              {profile.name || "Nguyen Ba Ky"}
+              {profile.firstName + " " + profile.lastName || "Nguyen Ba Ky"}
             </h2>
             <div className="mt-4 md:flex">
               <button className="bg-red-500 text-white py-2 px-4 md:w-full rounded-lg mr-2 hover:bg-red-600">
@@ -111,12 +124,14 @@ const RecruiterProfile = () => {
                 <input
                   type="text"
                   name="fullName"
-                  value={profile.fullName || ""}
+                  value={(profile.firstName && profile.lastName) || ""}
                   onChange={handleInputChange}
                   className="border p-2 rounded w-full"
                 />
               ) : (
-                <span>{profile.fullName || "Nguyen Ba Ky"}</span>
+                <span>
+                  {profile.firstName + " " + profile.lastName || "Nguyen Ba Ky"}
+                </span>
               )}
             </p>
             <p>
@@ -186,7 +201,7 @@ const RecruiterProfile = () => {
                   className="border p-2 rounded w-full"
                 />
               ) : (
-                <span>{profile.companyName || "Axon"}</span>
+                <span>{profile.company_name || "Axon"}</span>
               )}
             </p>
           </div>
